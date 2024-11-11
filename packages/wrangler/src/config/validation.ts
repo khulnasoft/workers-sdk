65d8f30f97442f24e7f206b228c5837b43614591
Taylor Lee <tlee@cloudflare.com>
2024-11-11 08:41:15 -0800

    feat(wrangler): create separate config for workers_dev_previews
    The current Preview URLs (beta) feature routes to version preview urls based on the status of the `workers_dev` config value. Beta users have requested the ability to enable deployment urls and preview urls separately on `workers.dev`, and the new `previews_enabled` field of the enable-subdomain API will allow that. This change separates the `workers_dev` and `workers_dev_previews` behavior during `wrangler triggers deploy` and `wrangler versions upload`. `wrangler_dev_previews` defaults to true, and does not implicitly depend on routes the way `wrangler_dev` does.

--- packages/wrangler/src/config/validation.ts
+++ packages/wrangler/src/config/validation.ts
@@ -1122,6 +1122,15 @@ function normalizeAndValidateEnvironment(
 		undefined
 	);
 
+	const workers_dev_previews = inheritable(
+		diagnostics,
+		topLevelEnv,
+		rawEnv,
+		"workers_dev_previews",
+		isBoolean,
+		true
+	);
+
 	const { deprecatedUpload, ...build } = normalizeAndValidateBuild(
 		diagnostics,
 		rawEnv,
@@ -1256,6 +1265,7 @@ function normalizeAndValidateEnvironment(
 		placement: normalizeAndValidatePlacement(diagnostics, topLevelEnv, rawEnv),
 		build,
 		workers_dev,
+		workers_dev_previews,
 		// Not inherited fields
 		vars: notInheritable(
 			diagnostics,
